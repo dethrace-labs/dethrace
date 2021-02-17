b8a85aeed64728d781a3b1cc3caa7115e85abdb2
OmniBlade <nadia_xy@hotmail.com>
2021-02-17 23:39:14 +0000

    Adds startup code to enable printing to cmd prompt.
    If game is started from command line, debug output prints to the console.

--- src/DETHRACE/main.c
+++ src/DETHRACE/main.c
@@ -7,6 +7,23 @@
 extern int original_main(int pArgc, char* pArgv[]);
 
 int main(int argc, char* argv[]) {
+#ifdef _WIN32
+    /* Attach to the console that started us if any */
+    if (AttachConsole(ATTACH_PARENT_PROCESS)) {
+        /* We attached successfully, lets redirect IO to the consoles handles if not already redirected */
+        if (_fileno(stdout) == -2 || _get_osfhandle(fileno(stdout)) == -2) {
+            freopen("CONOUT$", "w", stdout);
+        }
+
+        if (_fileno(stderr) == -2 || _get_osfhandle(fileno(stderr)) == -2) {
+            freopen("CONOUT$", "w", stderr);
+        }
+
+        if (_fileno(stdin) == -2 || _get_osfhandle(fileno(stdin)) == -2) {
+            freopen("CONIN$", "r", stdin);
+        }
+    }
+#endif
 
     Harness_Init(argv[0], &OpenGLRenderer);
 
