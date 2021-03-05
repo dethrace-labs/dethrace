ca50649a1209c2bef8acf513ca9ffa18b607d717
OmniBlade <nadia_xy@hotmail.com>
2021-03-05 23:03:33 +0000

    Fixes linking issues on macOS.
    macOS linker seems to have issue with linking when only symbols are default initialised globals.

--- src/BRSRC13/CORE/PIXELMAP/fontptrs.c
+++ src/BRSRC13/CORE/PIXELMAP/fontptrs.c
@@ -1,6 +1,7 @@
 #include "fontptrs.h"
+#include <stddef.h>
 
-br_font* BrFontProp4x6;
-br_font* BrFontProp7x9;
-br_font* BrFontFixed3x5;
+br_font* BrFontProp4x6 = NULL;
+br_font* BrFontProp7x9 = NULL;
+br_font* BrFontFixed3x5 = NULL;
 char rscid[50];
--- src/BRSRC13/CORE/V1DB/def_mdl.c
+++ src/BRSRC13/CORE/V1DB/def_mdl.c
@@ -2,5 +2,5 @@
 
 br_face default_model_faces[12];
 br_vertex default_model_vertices[8];
-br_model _BrDefaultModel;
+br_model _BrDefaultModel = {0};
 char rscid[49];
