baafd76a096844ac452968263f905e20145a5540
Jeff Harris <jeff@1amstudios.com>
2021-04-14 06:01:19 +1200

    Workflow test (#47)
    * Adds first test of create release when tag is pushed
    * fix pixelmap read warnings

--- src/BRSRC13/CORE/PIXELMAP/pmfile.c
+++ src/BRSRC13/CORE/PIXELMAP/pmfile.c
@@ -8,10 +8,17 @@
 
 #include <stddef.h>
 
-br_file_struct br_old_pixelmap_F;
 br_file_enum pixelmap_type_F;
 
-br_file_struct_member br_old_pixelmap_FM[7];
+br_file_struct_member br_old_pixelmap_FM[7] = {
+    { 13u, offsetof(br_pixelmap, type), "type", NULL },
+    { 3u, offsetof(br_pixelmap, row_bytes), "row_bytes", NULL },
+    { 3u, offsetof(br_pixelmap, width), "width", NULL },
+    { 3u, offsetof(br_pixelmap, height), "height", NULL },
+    { 3u, offsetof(br_pixelmap, origin_x), "origin_x", NULL },
+    { 3u, offsetof(br_pixelmap, origin_y), "origin_y", NULL },
+    { 17u, offsetof(br_pixelmap, identifier), "identifier", NULL }
+};
 
 br_file_struct_member br_pixelmap_FM[8] = {
     { 13u, offsetof(br_pixelmap, type), "type", NULL },
@@ -20,19 +27,19 @@ br_file_struct_member br_pixelmap_FM[8] = {
     { 3u, offsetof(br_pixelmap, height), "height", NULL },
     { 3u, offsetof(br_pixelmap, origin_x), "origin_x", NULL },
     { 3u, offsetof(br_pixelmap, origin_y), "origin_y", NULL },
+    { 3u, offsetof(br_pixelmap, mip_offset), "mip_offset", NULL },
     { 17u, offsetof(br_pixelmap, identifier), "identifier", NULL },
-    //not sure about this one...
-    { 31436u, 7u, &br_pixelmap_FM, (void*)0x44 }
+
 };
-br_file_struct br_pixelmap_F = { "br_pixelmap", 7u, br_pixelmap_FM, sizeof(br_pixelmap) };
+br_file_struct br_old_pixelmap_F = { "br_old_pixelmap", 7u, br_old_pixelmap_FM, sizeof(br_pixelmap) };
+br_file_struct br_pixelmap_F = { "br_pixelmap", 8u, br_pixelmap_FM, sizeof(br_pixelmap) };
 
 br_file_enum_member pixelmap_type_FM[15];
 br_chunks_table_entry PixelmapLoadEntries[5] = {
     { 0u, 0u, &FopRead_END },
-    { 3u, 0u, &FopRead_PIXELMAP },
+    { 3u, 0u, &FopRead_OLD_PIXELMAP },
     { 33u, 0u, &FopRead_PIXELS },
-    { 34u, 0u, &FopRead_ADD_MAP },
-    { 4u, 168u, NULL }
+    { 34u, 0u, &FopRead_ADD_MAP }
 };
 br_chunks_table PixelmapLoadTable = { 4, PixelmapLoadEntries };
 
@@ -51,21 +58,21 @@ int FopWrite_PIXELMAP(br_datafile* df, br_pixelmap* pixelmap) {
 int FopRead_OLD_PIXELMAP(br_datafile* df, br_uint_32 id, br_uint_32 length, br_uint_32 count) {
     br_pixelmap* pp;
     LOG_TRACE("(%p, %d, %d, %d)", df, id, length, count);
-    NOT_IMPLEMENTED();
+
+    pp = (br_pixelmap*)DevicePixelmapMemAllocate(3u, 0, 0, 0, 2);
+    df->res = pp;
+    df->prims->struct_read(df, &br_old_pixelmap_F, pp);
+    df->res = NULL;
+    pp->row_bytes = (pmTypeInfo[pp->type].bits >> 3) * pp->width;
+    DfPush(DF_PIXELMAP, pp, 1);
+    return 0;
 }
 
 // IDA: int __usercall FopRead_PIXELMAP@<EAX>(br_datafile *df@<EAX>, br_uint_32 id@<EDX>, br_uint_32 length@<EBX>, br_uint_32 count@<ECX>)
 int FopRead_PIXELMAP(br_datafile* df, br_uint_32 id, br_uint_32 length, br_uint_32 count) {
     br_pixelmap* pp;
     LOG_TRACE9("(%p, %d, %d, %d)", df, id, length, count);
-
-    pp = (br_pixelmap*)DevicePixelmapMemAllocate(3u, 0, 0, 0, 2);
-    df->res = pp;
-    df->prims->struct_read(df, &br_pixelmap_F, pp);
-    df->res = NULL;
-    pp->row_bytes = (pmTypeInfo[pp->type].bits >> 3) * pp->width;
-    DfPush(DF_PIXELMAP, pp, 1);
-    return 0;
+    NOT_IMPLEMENTED();
 }
 
 // IDA: int __usercall FopWrite_PIXELS@<EAX>(br_datafile *df@<EAX>, br_pixelmap *pixelmap@<EDX>)
