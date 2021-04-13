971bd31e4ea7cc3f50e7b84541d645aa5b613d2f
Jeff Harris <jeff@1amstudios.com>
2021-04-14 06:56:01 +1200

    workflow: tag release 8

---
+++ .github/scripts/build-linux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+if [ "${MATRIX_PLATFORM}" = "clang" ]
+then
+    CC=clang
+    CXX=clang++
+else
+    CC=gcc
+    CXX=g++
+fi
+
+# install deps
+sudo apt-get update -qq > /dev/null
+sudo apt-get install -qq -y libsdl2-dev > /dev/null
+
+# build
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_TESTS=ON -B build
+cmake --build build -- -j 4
+
+# run tests
+cd build
+ctest --verbose
