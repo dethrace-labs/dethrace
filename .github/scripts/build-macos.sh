5f8f20cda3e649cea2c0a7b2bd17c3788bd1bdb1
Jeff Harris <jeff@1amstudios.com>
2021-04-14 09:46:31 +1200

    Workflow #2 (#48)
    * Workflow changes #2

---
+++ .github/scripts/build-macos.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# install deps
+brew install SDL2
+
+# build
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_TESTS=ON -B build
+cmake --build build -- -j 4
+
+# package artifact
+tar -czvf darwin-amd64.tar.gz build/dethrace
---
+++ .github/scripts/build-windows.ps1
@@ -0,0 +1,19 @@
+if ($($Env:MATRIX_PLATFORM) -eq "x86") {
+    $sdl_path = "x86"
+} else {
+    $sdl_path = "x64"
+}
+
+# install deps
+Invoke-WebRequest -Uri https://www.libsdl.org/release/SDL2-devel-2.0.12-VC.zip -OutFile $Env:TEMP\SDL2-devel.zip
+Invoke-WebRequest -Uri https://github.com/ninja-build/ninja/releases/download/v1.10.1/ninja-win.zip -OutFile $Env:TEMP\ninja-win.zip
+Expand-Archive $Env:TEMP\SDL2-devel.zip -DestinationPath $Env:TEMP
+Expand-Archive $Env:TEMP\ninja-win.zip -DestinationPath $Env:TEMP\ninja
+echo "$Env:TEMP\ninja" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+
+# build
+cmake -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DSDL2=ON "-DSDL2_ROOT_DIR=$($Env:TEMP)\SDL2-2.0.12" -B build
+cmake --build build --config RelWithDebInfo
+
+# package artifact
+7z a windows-$Env:MATRIX_PLATFORM.zip build\dethrace.exe build/dethrace.pdb $Env:TEMP\SDL2-2.0.12\lib\$sdl_path\SDL2.dll
