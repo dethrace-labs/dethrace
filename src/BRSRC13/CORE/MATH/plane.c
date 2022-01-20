0bff2648c0dda15488dc99cca1685d8b900da76c
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-01-20 02:37:40 +0100

    brender: re-order assignment of variables
    Little nitpick patch, sorry :^)

--- src/BRSRC13/CORE/MATH/plane.c
+++ src/BRSRC13/CORE/MATH/plane.c
@@ -19,25 +19,25 @@ br_int_32 BrPlaneEquation(br_vector4* eqn, br_vector3* v0, br_vector3* v1, br_ve
     float d;
     float l;
 
+    ax = v1->v[0] - v0->v[0];
     ay = v1->v[1] - v0->v[1];
-    bz = v2->v[2] - v0->v[2];
     az = v1->v[2] - v0->v[2];
     bx = v2->v[0] - v0->v[0];
-    ax = v1->v[0] - v0->v[0];
     by = v2->v[1] - v0->v[1];
+    bz = v2->v[2] - v0->v[2];
     nx = ay * bz - az * by;
     ny = az * bx - ax * bz;
     nz = ax * by - ay * bx;
     l = nx * nx + ny * ny + nz * nz;
     l = BrFloatSqrt(l);
     if (isnan(l)) {
-        nx = 0.0;
-        ny = 0.0;
-        nz = 1.0;
+        nx = 0.f;
+        ny = 0.f;
+        nz = 1.f;
     } else {
-        nx = nx * (1.0f / l);
-        ny = ny * (1.0f / l);
-        nz = nz * (1.0f / l);
+        nx = nx * (1.f / l);
+        ny = ny * (1.f / l);
+        nz = nz * (1.f / l);
     }
     d = nx * v0->v[0] + ny * v0->v[1] + nz * v0->v[2];
     eqn->v[0] = nx;
