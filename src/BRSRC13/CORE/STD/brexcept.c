746f0ff64ed26b4ee4083b9119e0aacfb507ffd8
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-01-26 21:02:20 +0100

    brender: implement all brerxcept functions

--- src/BRSRC13/CORE/STD/brexcept.c
+++ src/BRSRC13/CORE/STD/brexcept.c
@@ -1,5 +1,10 @@
 #include "brexcept.h"
+
+#include "CORE/FW/resource.h"
+#include "CORE/FW/diag.h"
 #include "harness/trace.h"
+
+#include <string.h>
 
 void* exceptionValue;
 br_exception_handler* _BrExceptionHandler;
@@ -8,14 +13,24 @@ br_exception_handler* _BrExceptionHandler;
 br_exception_handler* _BrExceptionBegin() {
     br_exception_handler* h;
     LOG_TRACE("()");
-    NOT_IMPLEMENTED();
+
+    h = BrResAllocate(NULL, sizeof(br_exception_handler), BR_MEMORY_EXCEPTION_HANDLER);
+    h->prev = _BrExceptionHandler;
+    _BrExceptionHandler = h;
+    return h;
 }
 
 // IDA: void __cdecl _BrExceptionEnd()
 void _BrExceptionEnd() {
     br_exception_handler* old;
     LOG_TRACE("()");
-    NOT_IMPLEMENTED();
+
+    if (_BrExceptionHandler == NULL) {
+        BrFailure("Unhandled exception: %d");
+    }
+    old = _BrExceptionHandler;
+    _BrExceptionHandler = _BrExceptionHandler->prev;
+    BrResFree(old);
 }
 
 // IDA: void __cdecl _BrExceptionThrow(br_int_32 type, void *value)
@@ -23,17 +38,31 @@ void _BrExceptionThrow(br_int_32 type, void* value) {
     br_exception_handler h;
     br_exception_handler* old;
     LOG_TRACE("(%d, %p)", type, value);
-    NOT_IMPLEMENTED();
+
+    if (_BrExceptionHandler == NULL) {
+        BrFailure("Unhandled exception: %d");
+    }
+    memcpy(&h, _BrExceptionHandler, sizeof(br_exception_handler));
+    old = _BrExceptionHandler;
+    _BrExceptionHandler = _BrExceptionHandler->prev;
+    BrResFree(old);
+    exceptionValue = value;
+    longjmp(h.context, type);
 }
 
 // IDA: br_exception __cdecl _BrExceptionValueFetch(br_exception type, void **evp)
 br_exception _BrExceptionValueFetch(br_exception type, void** evp) {
     LOG_TRACE("(%d, %p)", type, evp);
-    NOT_IMPLEMENTED();
+
+    if (type != 0 && evp != NULL) {
+        *evp = exceptionValue;
+    }
+    return type;
 }
 
 // IDA: void* __cdecl _BrExceptionResource()
 void* _BrExceptionResource() {
     LOG_TRACE("()");
-    NOT_IMPLEMENTED();
+
+    return _BrExceptionHandler;
 }
