91f263db158f7c85c9b62e14ca47ebb6441a0fec
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-03-22 10:02:25 +0100

    No LOG_WARN in Windows

--- src/harness/os/windows.c
+++ src/harness/os/windows.c
@@ -212,7 +212,7 @@ FILE* OS_fopen(const char* pathname, const char* mode) {
     f = NULL;
     err = fopen_s(&f, pathname, mode);
     if (err != 0) {
-        LOG_WARN("Failed to open \"%s\"", pathname);
+        fprintf(stderr, "Failed to open \"%s\"", pathname);
     }
 
     return f;
