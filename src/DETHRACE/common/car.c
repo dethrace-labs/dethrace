bc99ddcffa8c3b987f0b34cfc0ba9fdae3fc4ac3
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-11-16 01:38:08 +0100

    Compare against floats in MoveAndCollideNonCar

--- src/DETHRACE/common/car.c
+++ src/DETHRACE/common/car.c
@@ -1370,11 +1370,11 @@ void MoveAndCollideNonCar(tNon_car_spec* non_car, br_scalar dt) {
     LOG_TRACE("(%p, %f)", non_car, dt);
 
     car_info = &non_car->collision_info;
-    if (car_info->water_d != 10000.0f) {
+    if (car_info->water_d != 10000.f) {
         TestAutoSpecialVolume(&non_car->collision_info);
     }
     MungeSpecialVolume(&non_car->collision_info);
-    if (car_info->dt >= 0.0) {
+    if (car_info->dt >= 0.f) {
         dt = car_info->dt;
     }
     NonCarCalcForce(non_car, dt);
