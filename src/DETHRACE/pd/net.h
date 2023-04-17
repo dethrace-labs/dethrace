d8046f2b73db8e82b8006393ffdfa2baef63fbe4
Dethrace Labs <78985374+dethrace-labs@users.noreply.github.com>
2023-04-18 11:50:41 +1200

    tidy

--- src/DETHRACE/pd/net.h
+++ src/DETHRACE/pd/net.h
@@ -40,17 +40,17 @@
 
 // BOOL hmiIPXPostListen(_IPX_ECB* pECB_ptr, tU32 pOffset);
 
-// int hmiIPXGetData(char* pData, tU32 wDSize);
+// BOOL hmiIPXGetData(PSTR pData, tU32 wDSize);
 
-// void hmiIPXSendPacket(_IPX_ECB* sECB, _IPX_ECB** pPacket, char* pHeader, W32 wSize);
+// void hmiIPXSendPacket(_IPX_ECB* sECB, _IPX_ECB** pPacket, PSTR pHeader, W32 wSize);
 
-// int hmiIPXSendDataDirect(char* pHeader, W32 wHSize, char* pData, W32 wDSize, _NETNOW_NODE_ADDR* sNode);
+// BOOL hmiIPXSendDataDirect(PSTR pHeader, W32 wHSize, PSTR pData, W32 wDSize, _NETNOW_NODE_ADDR* sNode);
 
 // void hmiIPXGetInternetworkAddr(_IPX_INTERNET_ADDR* sInterworkAddr);
 
 // void hmiIPXGetLocalTarget(_IPX_LOCAL_TARGET* sNetworkAddr);
 
-// int AllocateRealMem(W32 wSize, char** pPtr, W32* pSegment, tU16* pSelector);
+// BOOL AllocateRealMem(W32 wSize, PSTR* pPtr, W32* pSegment, tU16* pSelector);
 
 // BOOL FreeRealMem(tU16 pSelector);
 
