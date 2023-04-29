35ddeefcfb82f460947589bf0f521e01cd141eb5
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-04-29 22:56:16 +0200

    Fix -Wstrict-prototypes warnings in S3

---
+++ src/S3/include/s3/s3_brender.h
@@ -0,0 +1,14 @@
+#ifndef S3_S3_BRENDER_H
+#define S3_S3_BRENDER_H
+
+#include "s3.h"
+
+#include "brender/brender.h"
+
+tS3_sound_source_ptr S3CreateSoundSourceBR(br_vector3* pPosition, br_vector3* pVelocity, tS3_outlet_ptr pBound_outlet);
+
+void S3BindListenerPositionBRender(br_vector3* pos);
+void S3BindListenerVelocityBRender(br_vector3* vel);
+void S3BindListenerLeftBRender(br_vector3* left);
+
+#endif /* S3_S3_BRENDER_H */
