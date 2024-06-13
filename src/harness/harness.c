99eb995ca4222051092f3fe43fff81f27f3171db
Jeff Harris <jeff@1amstudios.com>
2024-06-14 10:19:13 +1200

    tidy root dir comparison

--- src/harness/harness.c
+++ src/harness/harness.c
@@ -181,7 +181,8 @@ void Harness_Init(int* argc, char* argv[]) {
     } else {
         root_dir = OS_Dirname(argv[0]);
     }
-    if (root_dir != NULL && strlen(root_dir) > 1) {
+    // if root_dir is empty or ".", no need to chdir
+    if (root_dir != NULL && root_dir != '\0' && root_dir != '.') {
         printf("Using root directory: %s\n", root_dir);
         result = chdir(root_dir);
         if (result != 0) {
