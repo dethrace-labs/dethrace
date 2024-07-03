efb7a0df74ee0435bc6a2858b663292590f1b1c1
Dethrace Labs <78985374+dethrace-labs@users.noreply.github.com>
2024-07-03 13:37:17 +1200

    replaces icon with our own image

--- packaging/macos/generate_icns.sh
+++ packaging/macos/generate_icns.sh
@@ -1,16 +1,40 @@
 #!/bin/sh
+set -e
 
-ICON_PATH=source.png
+SOURCE_IMAGE=$1
+TEMP_ICON_IMAGE=icon.png
+
+# resize to 412x with black canvas
+convert ${SOURCE_IMAGE} -resize 412x412 -background Black -gravity center -extent 412x412 ${TEMP_ICON_IMAGE}
+
+# add rounded corners
+convert ${TEMP_ICON_IMAGE} \
+  \( +clone  -alpha extract \
+    -draw 'fill black polygon 0,0 0,50 50,0 fill white circle 50,50 50,0' \
+    \( +clone -flip \) -compose Multiply -composite \
+    \( +clone -flop \) -compose Multiply -composite \
+  \) -alpha off -compose CopyOpacity -composite ${TEMP_ICON_IMAGE}
+
+# add margin
+convert ${TEMP_ICON_IMAGE} -bordercolor transparent -border 40x40 ${TEMP_ICON_IMAGE}
+
+# add drop shadow
+convert ${TEMP_ICON_IMAGE} \
+  \( +clone -background black -shadow 100x5+0+0 \) +swap \
+  -background none -layers merge +repage ${TEMP_ICON_IMAGE}
+
+
 rm -r dethrace.iconset || true
 mkdir -p dethrace.iconset
-sips -z 16 16     $ICON_PATH --out dethrace.iconset/icon_16x16.png
-sips -z 32 32     $ICON_PATH --out dethrace.iconset/icon_16x16@2x.png
-sips -z 32 32     $ICON_PATH --out dethrace.iconset/icon_32x32.png
-sips -z 64 64     $ICON_PATH --out dethrace.iconset/icon_32x32@2x.png
-sips -z 128 128   $ICON_PATH --out dethrace.iconset/icon_128x128.png
-sips -z 256 256   $ICON_PATH --out dethrace.iconset/icon_128x128@2x.png
-sips -z 256 256   $ICON_PATH --out dethrace.iconset/icon_256x256.png
-sips -z 512 512   $ICON_PATH --out dethrace.iconset/icon_256x256@2x.png
-sips -z 512 512   $ICON_PATH --out dethrace.iconset/icon_512x512.png
-sips -z 1024 1024 $ICON_PATH --out dethrace.iconset/icon_512x512@2x.png
+sips -z 16 16     ${TEMP_ICON_IMAGE} --out dethrace.iconset/icon_16x16.png
+sips -z 32 32     ${TEMP_ICON_IMAGE} --out dethrace.iconset/icon_16x16@2x.png
+sips -z 32 32     ${TEMP_ICON_IMAGE} --out dethrace.iconset/icon_32x32.png
+sips -z 64 64     ${TEMP_ICON_IMAGE} --out dethrace.iconset/icon_32x32@2x.png
+sips -z 128 128   ${TEMP_ICON_IMAGE} --out dethrace.iconset/icon_128x128.png
+sips -z 256 256   ${TEMP_ICON_IMAGE} --out dethrace.iconset/icon_128x128@2x.png
+sips -z 256 256   ${TEMP_ICON_IMAGE} --out dethrace.iconset/icon_256x256.png
+sips -z 512 512   ${TEMP_ICON_IMAGE} --out dethrace.iconset/icon_256x256@2x.png
+sips -z 512 512   ${TEMP_ICON_IMAGE} --out dethrace.iconset/icon_512x512.png
+sips -z 1024 1024 ${TEMP_ICON_IMAGE} --out dethrace.iconset/icon_512x512@2x.png
 iconutil -c icns dethrace.iconset
+rm -r dethrace.iconset ${TEMP_ICON_IMAGE}
--- packaging/windows/generate_ico.sh
+++ packaging/windows/generate_ico.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+set -e
 
-convert -background transparent source.png -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "dethrace.ico"
+SOURCE_IMAGE=$1
+TEMP_ICON_IMAGE=icon.png
+
+# resize to 412x with black canvas
+convert ${SOURCE_IMAGE} -resize 412x412 -background Black -gravity center -extent 412x412 ${TEMP_ICON_IMAGE}
+# convert to ico format
+convert -background transparent ${TEMP_ICON_IMAGE} -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "dethrace.ico"
+
+rm ${TEMP_ICON_IMAGE}
