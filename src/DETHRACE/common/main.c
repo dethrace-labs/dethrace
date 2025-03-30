31c1e604efd8b36138c07c0d07063edfd758449d
Dethrace Labs <78985374+dethrace-labs@users.noreply.github.com>
2025-03-30 15:32:02 +1300

    fixes crash when closing window in some cases

--- src/DETHRACE/common/main.c
+++ src/DETHRACE/common/main.c
@@ -39,6 +39,12 @@ void QuitGame(void) {
         DRS3ShutDown();
     }
     if (gBr_initialized) {
+#ifdef DETHRACE_FIX_BUGS
+        // In 3dfx mode, we need direct pixel access before calling `ClearEntireScreen`
+        if (harness_game_config.opengl_3dfx_mode) {
+            PDLockRealBackScreen(1);
+        }
+#endif
         ClearEntireScreen();
     }
     PDRevertPalette();
