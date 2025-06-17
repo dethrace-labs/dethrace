2512e9a8cf0954e70131dd26a73fc0cd4e59c21b
Dethrace Labs <78985374+dethrace-labs@users.noreply.github.com>
2025-06-17 16:10:26 +1200

    adds library function addresses for crt and brender

---
+++ src/library_msvc.h
@@ -0,0 +1,422 @@
+// LIBRARY: CARM95 0x455730
+// _iswxdigit
+
+// LIBRARY: CARM95 0x4a61ca
+// _WinMain@16
+
+// LIBRARY: CARM95 0x4ea660
+// _strtok
+
+// LIBRARY: CARM95 0x4ea8a0
+// _asin
+
+// LIBRARY: CARM95 0x4ea8aa
+// _acos
+
+// LIBRARY: CARM95 0x4ea8b1
+// _atan
+
+// LIBRARY: CARM95 0x4ea8b8
+// _atan2
+
+// LIBRARY: CARM95 0x4ea8f0
+// _sprintf
+
+// LIBRARY: CARM95 0x4eaa10
+// _memcpy
+
+// LIBRARY: CARM95 0x4eab60
+// _pow
+
+// LIBRARY: CARM95 0x4eab6a
+// _log
+
+// LIBRARY: CARM95 0x4eab74
+// _log10
+
+// LIBRARY: CARM95 0x4eab7b
+// _exp
+
+// LIBRARY: CARM95 0x4eabe0
+// _fclose
+
+// LIBRARY: CARM95 0x4eacd0
+// _fprintf
+
+// LIBRARY: CARM95 0x4ead80
+// _qsort
+
+// LIBRARY: CARM95 0x4eafe0
+// _shortsort
+
+// LIBRARY: CARM95 0x4eb070
+// _swap
+
+// LIBRARY: CARM95 0x4eb0d0
+// _ldexp
+
+// LIBRARY: CARM95 0x4eb3e0
+// _rand
+
+// LIBRARY: CARM95 0x4eb430
+// _floor
+
+// LIBRARY: CARM95 0x4eb5a0
+// _ceil
+
+// LIBRARY: CARM95 0x4eb710
+// _fread
+
+// LIBRARY: CARM95 0x4eb8e0
+// _fwrite
+
+// LIBRARY: CARM95 0x4ebb00
+// _fseek
+
+// LIBRARY: CARM95 0x4ebc30
+// _sscanf
+
+// LIBRARY: CARM95 0x4ebd00
+// _fgets
+
+// LIBRARY: CARM95 0x4ebf40
+// _fopen
+
+// LIBRARY: CARM95 0x4ebf70
+// _strrchr
+
+// LIBRARY: CARM95 0x4ebfa0
+// _fscanf
+
+// LIBRARY: CARM95 0x4ec030
+// _ungetc
+
+// LIBRARY: CARM95 0x4ec350
+// _strchr
+
+// LIBRARY: CARM95 0x4ec410
+// _strstr
+
+// LIBRARY: CARM95 0x4ec490
+// _fgetc
+
+// LIBRARY: CARM95 0x4ec510
+// _fmod
+
+// LIBRARY: CARM95 0x4ec550
+// _fputs
+
+// LIBRARY: CARM95 0x4ec6e0
+// _strncmp
+
+// LIBRARY: CARM95 0x4ec720
+// _strpbrk
+
+// LIBRARY: CARM95 0x4ec760
+// _strncpy
+
+// LIBRARY: CARM95 0x4ec860
+// _rewind
+
+// LIBRARY: CARM95 0x4ec920
+// _ctime
+
+// LIBRARY: CARM95 0x4ec970
+// _time
+
+// LIBRARY: CARM95 0x4ecb00
+// _malloc
+
+// LIBRARY: CARM95 0x4ecfc0
+// _realloc_help
+
+// LIBRARY: CARM95 0x4ed9a0
+// _CheckBytes
+
+// LIBRARY: CARM95 0x4ee4a0
+// _memchr
+
+// LIBRARY: CARM95 0x4ee550
+// _printf
+
+// LIBRARY: CARM95 0x4ee5e0
+// _fputc
+
+// LIBRARY: CARM95 0x4ee680
+// _strtol
+
+// LIBRARY: CARM95 0x4ee6b0
+// _strtoxl
+
+// LIBRARY: CARM95 0x4eea40
+// _strtoul
+
+// LIBRARY: CARM95 0x4eee80
+// _CrtMessageWindow
+
+// LIBRARY: CARM95 0x4ef1b0
+// _ftell
+
+// LIBRARY: CARM95 0x4ef4b0
+// _exit
+
+// LIBRARY: CARM95 0x4ef4f0
+// _doexit
+
+// LIBRARY: CARM95 0x4ef630
+// _rename
+
+// LIBRARY: CARM95 0x4ef690
+// _remove
+
+// LIBRARY: CARM95 0x4ef6f0
+// _tolower
+
+// LIBRARY: CARM95 0x4efac0
+// _abort
+
+// LIBRARY: CARM95 0x4efaf0
+// _getenv
+
+// LIBRARY: CARM95 0x4efbe0
+// _strtod
+
+// LIBRARY: CARM95 0x4efd40
+// _vsprintf
+
+// LIBRARY: CARM95 0x4f0717
+// _rtforsqrtinf
+
+// LIBRARY: CARM95 0x4f071c
+// _rtforsqrtzero
+
+// LIBRARY: CARM95 0x4f09f5
+// _rtforatn20
+
+// LIBRARY: CARM95 0x4f0a11
+// _rtforatn200
+
+// LIBRARY: CARM95 0x4f0a18
+// _rtforatnby0
+
+// LIBRARY: CARM95 0x4f0a1e
+// _rtsignpiby2
+
+// LIBRARY: CARM95 0x4f1a60
+// _write_char
+
+// LIBRARY: CARM95 0x4f1ae0
+// _write_multi_char
+
+// LIBRARY: CARM95 0x4f1b30
+// _write_string
+
+// LIBRARY: CARM95 0x4f1b90
+// _get_int_arg
+
+// LIBRARY: CARM95 0x4f1bb0
+// _get_int64_arg
+
+// LIBRARY: CARM95 0x4f1be0
+// _get_short_arg
+
+// LIBRARY: CARM95 0x4f1c6e
+// _rtfor0to0
+
+// LIBRARY: CARM95 0x4f1ca4
+// _rtforln0
+
+// LIBRARY: CARM95 0x4f1cb4
+// _rtforloginf
+
+// LIBRARY: CARM95 0x4f1cc8
+// _rtforyto0
+
+// LIBRARY: CARM95 0x4f1ccd
+// _rtfor0tox
+
+// LIBRARY: CARM95 0x4f1ce8
+// _rtfor0toneg
+
+// LIBRARY: CARM95 0x4f1d32
+// _expbigret
+
+// LIBRARY: CARM95 0x4f1d51
+// _rtforexpinf
+
+// LIBRARY: CARM95 0x4f1da1
+// _isintTOS
+
+// LIBRARY: CARM95 0x4f1dc6
+// _isintTOSret
+
+// LIBRARY: CARM95 0x4f1dd5
+// _usepowhlp
+
+// LIBRARY: CARM95 0x4f1fe0
+// _fflush
+
+// LIBRARY: CARM95 0x4f2140
+// _flsall
+
+// LIBRARY: CARM95 0x4f3b90
+// _memcpy_0
+
+// LIBRARY: CARM95 0x4f55f0
+// _strlen
+
+// LIBRARY: CARM95 0x4f606f
+// _tossnan1
+
+// LIBRARY: CARM95 0x4f609d
+// _tossnan2
+
+// LIBRARY: CARM95 0x4f60a4
+// _tosnan2ret
+
+// LIBRARY: CARM95 0x4f60dc
+// _snan2
+
+// LIBRARY: CARM95 0x4f60e3
+// _nan2ret
+
+// LIBRARY: CARM95 0x4f7040
+// _asctime
+
+// LIBRARY: CARM95 0x4f71a0
+// _store_dt
+
+// LIBRARY: CARM95 0x4f71e0
+// _localtime
+
+// LIBRARY: CARM95 0x4f75a0
+// _memset
+
+// LIBRARY: CARM95 0x4f8e80
+// _toupper
+
+// LIBRARY: CARM95 0x4f9020
+// _xtoa
+
+// LIBRARY: CARM95 0x4f90e0
+// _strcpy
+
+// LIBRARY: CARM95 0x4f90f0
+// _strcat
+
+// LIBRARY: CARM95 0x4f93b0
+// _raise
+
+// LIBRARY: CARM95 0x4f95d0
+// _siglookup
+
+// LIBRARY: CARM95 0x4f9b00
+// _strncnt
+
+// LIBRARY: CARM95 0x4f9b7c
+// _gu_return
+
+// LIBRARY: CARM95 0x4f9ba5
+// _uh_return
+
+// LIBRARY: CARM95 0x4f9bc3
+// _lu_top
+
+// LIBRARY: CARM95 0x4f9c00
+// _lu_done
+
+// LIBRARY: CARM95 0x4f9c30
+// _at_done
+
+// LIBRARY: CARM95 0x4f9e60
+// _xcptlookup
+
+// LIBRARY: CARM95 0x4f9ef0
+// _x_ismbbtype
+
+// LIBRARY: CARM95 0x4fa170
+// _parse_cmdline
+
+// LIBRARY: CARM95 0x4faac0
+// _getSystemCP
+
+// LIBRARY: CARM95 0x4fab50
+// _CPtoLCID
+
+// LIBRARY: CARM95 0x4fabf0
+// _setSBCS
+
+// LIBRARY: CARM95 0x4facc3
+// _lh_top
+
+// LIBRARY: CARM95 0x4fad17
+// _lh_continue
+
+// LIBRARY: CARM95 0x4fad22
+// _lh_dismiss
+
+// LIBRARY: CARM95 0x4fad29
+// _lh_bagit
+
+// LIBRARY: CARM95 0x4fad30
+// _lh_unwinding
+
+// LIBRARY: CARM95 0x4fad45
+// _lh_return
+
+// LIBRARY: CARM95 0x4fc210
+// _wctomb
+
+// LIBRARY: CARM95 0x4fcc00
+// _mbtowc
+
+// LIBRARY: CARM95 0x4fcda0
+// _isspace
+
+// LIBRARY: CARM95 0x4fdae0
+// _cvtdate
+
+// LIBRARY: CARM95 0x4fdcf0
+// _gmtime
+
+// LIBRARY: CARM95 0x4fdfa0
+// _wcslen
+
+// LIBRARY: CARM95 0x4fe450
+// _strncnt_0
+
+// LIBRARY: CARM95 0x4fe830
+// _findenv
+
+// LIBRARY: CARM95 0x4fe8d0
+// _copy_environ
+
+// LIBRARY: CARM95 0x4ff930
+// _$I10_OUTPUT
+
+// LIBRARY: CARM95 0x500120
+// _atol
+
+// LIBRARY: CARM95 0x500240
+// _strcmp
+
+// LIBRARY: CARM95 0x5002d0
+// _wcstombs
+
+// LIBRARY: CARM95 0x500630
+// _wcsncnt
+
+// LIBRARY: CARM95 0x502f60
+// _feof
+
+// LIBRARY: CARM95 0x503140
+// _longjmp
+
+// LIBRARY: CARM95 0x50315b
+// _lj_local_unwind
+
+// LIBRARY: CARM95 0x503183
+// _lj_old_unwind
+
+// LIBRARY: CARM95 0x503190
+// _lj_no_unwind
