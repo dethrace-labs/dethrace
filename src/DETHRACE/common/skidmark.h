39c4051b91e27b2092a6f86198e3ff48d9eb88f2
Dethrace Labs <78985374+dethrace-labs@users.noreply.github.com>
2025-09-19 10:15:59 +1200

    SkidMark 98% matching

--- src/DETHRACE/common/skidmark.h
+++ src/DETHRACE/common/skidmark.h
@@ -3,9 +3,9 @@
 
 #include "dr_types.h"
 
+extern tSkid gSkids[100];
 extern char* gBoring_material_names[2];
 extern char* gMaterial_names[2];
-extern tSkid gSkids[100];
 
 void StretchMark(tSkid* pMark, br_vector3* pFrom, br_vector3* pTo, br_scalar pTexture_start);
 
