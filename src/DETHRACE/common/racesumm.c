f8649ff85472fd5bead6b3e83817183a2893bdac
Dethrace Labs <78985374+dethrace-labs@users.noreply.github.com>
2025-10-15 08:06:42 +1300

    DamageScrnUp matching

--- src/DETHRACE/common/racesumm.c
+++ src/DETHRACE/common/racesumm.c
@@ -1088,18 +1088,19 @@ int DamageScrnUp(int* pCurrent_choice, int* pCurrent_mode) {
             *pCurrent_mode = 1;
             *pCurrent_choice = 2;
         } else {
-            new_difference = 1000;
+            difference = 1000;
             new_selection = gWreck_selected;
             for (i = 0; i < gWreck_count; i++) {
                 if (gWreck_array[gWreck_selected].pos_y - 1.f == gWreck_array[i].pos_y) {
                     if (gWreck_array[i].pos_x == gWreck_array[gWreck_selected].pos_x) {
                         new_selection = i;
                         break;
-                    }
-                    difference = abs((int)(gWreck_array[i].pos_x - gWreck_array[gWreck_selected].pos_x));
-                    if (difference < new_difference) {
-                        new_selection = i;
-                        new_difference = difference;
+                    } else {
+                        new_difference = abs((int)(gWreck_array[i].pos_x - gWreck_array[gWreck_selected].pos_x));
+                        if (new_difference < difference) {
+                            difference = new_difference;
+                            new_selection = i;
+                        }
                     }
                 }
             }
