6ad87f701209f69a43548ec3f172a167976521f6
Jeff H <jeff@1amstudios.com>
2026-03-28 09:19:35 +1300

    revert over optimized matching

--- src/DETHRACE/common/pedestrn.c
+++ src/DETHRACE/common/pedestrn.c
@@ -3771,11 +3771,11 @@ void DisposePedestrians(void) {
     }
     for (i = 0; i < COUNT_OF(gPed_gib_materials); i++) {
         for (j = 0; j < gPed_size_counts[i]; j++) {
-            BrMapRemove((gPed_gib_materials + i)->materials[j * 1]->colour_map);
-            BrPixelmapFree((gPed_gib_materials + i)->materials[j * 1]->colour_map);
-            (gPed_gib_materials + i)->materials[j * 1]->colour_map = NULL;
-            BrMaterialRemove((gPed_gib_materials + i)->materials[j * 1]);
-            BrMaterialFree((gPed_gib_materials + i)->materials[j * 1]);
+            BrMapRemove(gPed_gib_materials[i].materials[j * 1]->colour_map);
+            BrPixelmapFree(gPed_gib_materials[i].materials[j * 1]->colour_map);
+            gPed_gib_materials[i].materials[j * 1]->colour_map = NULL;
+            BrMaterialRemove(gPed_gib_materials[i].materials[j * 1]);
+            BrMaterialFree(gPed_gib_materials[i].materials[j * 1]);
         }
     }
 
