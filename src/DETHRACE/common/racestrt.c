ed10dd2624dcbf5853b600608867ea3c622c982e
Jeff Harris <jeff@1amstudios.com>
2026-03-28 07:01:21 +1300

    Match PartsShopGoAhead (#643)
    * Match PartsShopGoAhead
    
    AI generated
    
    * revert the over-eager matching

--- src/DETHRACE/common/racestrt.c
+++ src/DETHRACE/common/racestrt.c
@@ -955,8 +955,9 @@ int PartsShopGoAhead(int* pCurrent_choice, int* pCurrent_mode) {
         RemoveTransientBitmaps(1);
         DontLetFlicFuckWithPalettes();
         TurnFlicTransparencyOn();
+        flic_index = gStart_interface_spec->pushed_flics[*pCurrent_choice].flic_index;
         RunFlicAt(
-            gStart_interface_spec->pushed_flics[*pCurrent_choice].flic_index,
+            flic_index,
             gStart_interface_spec->pushed_flics[*pCurrent_choice].x[gGraf_data_index],
             gStart_interface_spec->pushed_flics[*pCurrent_choice].y[gGraf_data_index]);
         TurnFlicTransparencyOff();
@@ -980,9 +981,9 @@ int PartsShopGoAhead(int* pCurrent_choice, int* pCurrent_mode) {
         return 0;
     } else if (*pCurrent_mode == 1) {
         AddToFlicQueue(
-            gStart_interface_spec->flicker_on_flics[4].flic_index,
-            gStart_interface_spec->flicker_on_flics[4].x[gGraf_data_index],
-            gStart_interface_spec->flicker_on_flics[4].y[gGraf_data_index],
+            gStart_interface_spec->pushed_flics[4].flic_index,
+            gStart_interface_spec->pushed_flics[4].x[gGraf_data_index],
+            gStart_interface_spec->pushed_flics[4].y[gGraf_data_index],
             1);
         DoExchangePart();
         return 0;
