c7440431011e2632ea7cffd873a4a58c65f6fa98
Anonymous Maarten <madebr@users.noreply.github.com>
2026-04-02 01:59:42 +0200

    Fix UBSAN runtime errors (#805)
    * Fix ubsan runtime error: member access within null pointer
    
    Complete warning:
    runtime error: member access within null pointer of type 'const struct ma_engine'
    
    The code was doing something like this: `&(NULL)->member`
    
    * Fix ubsan runtime error: left shift of 255 by 24 places cannot be represented in type 'int'
    
    * Fix ubsan warning where null pointer was passed as argument to memcpy
    
    Full warning:
    untime error: null pointer passed as argument 2, which is declared to never be null
    
    * Fix ubsan warning error: left shift of 1 by 41 places cannot be represented in type 'int'
    
    * Modify gMr_chunky2 as tU8 pointer
    
    This avoids ubsan tPipe_chunk alignment checks
    
    * Avoid unaligned write during RLE encoding of cockpit
    
    * Move gMr_chunky2 pointer using tU8 pointer to work around alignment requirements
    
    * Shifting negative number is ubsan
    
    * Avoid shifting bit into sign bit of signed number
    
    * Update BRender

--- src/DETHRACE/common/spark.c
+++ src/DETHRACE/common/spark.c
@@ -145,10 +145,17 @@ br_matrix34 gSmoke_camera_to_world;
 // Bugfix: At higher FPS, `CreatePuffOfSmoke` is called too often and causes smoke cirlces to be recycled too quickly so assume around 25fps
 #define SMOKE_COLUMN_NEW_PUFF_INTERVAL 30
 
+#ifdef DETHRACE_FIX_BUGS
+#define TEST_BIT(var, pos) (var & (1u << pos))
+#define SET_BIT(var, pos) (var |= (1u << pos))
+#define FLIP_BIT(var, pos) (var ^= (1u << pos))
+#define CLEAR_BIT(var, pos) (var &= ~(1u << pos))
+#else
 #define TEST_BIT(var, pos) (var & (1 << pos))
 #define SET_BIT(var, pos) (var |= (1 << pos))
 #define FLIP_BIT(var, pos) (var ^= (1 << pos))
 #define CLEAR_BIT(var, pos) (var &= ~(1 << pos))
+#endif
 
 // IDA: void __cdecl DrawDot(br_scalar z, tU8 *scr_ptr, tU16 *depth_ptr, tU8 *shade_ptr)
 // FUNCTION: CARM95 0x00466310
@@ -2358,7 +2365,12 @@ void SmudgeCar(tCar_spec* pCar, int fire_point) {
                     if (BR_ALPHA(V11MODEL(model)->groups[group].vertex_colours[j]) != (int)ts) {
                         data[n].vertex_index = real_vertex_number;
                         data[n].light_index = (int)ts - BR_ALPHA(V11MODEL(model)->groups[group].vertex_colours[j]);
+#ifdef DETHRACE_FIX_BUGS
+                        // Fixes ubsan runtime error: shifting number in sign bit of signed number is UB
+                        V11MODEL(model)->groups[group].vertex_colours[j] = (unsigned)ts << 24;
+#else
                         V11MODEL(model)->groups[group].vertex_colours[j] = (int)ts << 24;
+#endif
                         if ((model->flags & BR_MODF_UPDATEABLE) != 0) {
                             model->vertices[V11MODEL(model)->groups[group].vertex_user[j]].index = (int)ts;
                         }
