096e4d48c01d03c88b4f148612d99dfa6c860962
Jeff H <jeff@1amstudios.com>
2026-04-10 13:08:18 +1200

    massage

--- src/DETHRACE/common/flicplay.c
+++ src/DETHRACE/common/flicplay.c
@@ -1045,35 +1045,27 @@ void DoColour256(tFlic_descriptor* pFlic_info, tU32 chunk_length) {
     for (i = 0; i < packet_count; i++) {
         skip_count = MemReadU8(&pFlic_info->data);
         change_count = MemReadU8(&pFlic_info->data);
-        if (!change_count) {
+        if (change_count != 0) {
             change_count = 256;
         }
         current_colour += skip_count;
         palette_pixels = (tU8*)gPalette_pixels + current_colour * sizeof(br_int_32);
-        for (j = 0;; j++) {
-            if (change_count <= j) {
-                break;
-            }
+        for (j = 0; j < change_count; j++) {
             red = MemReadU8(&pFlic_info->data);
             green = MemReadU8(&pFlic_info->data);
             blue = MemReadU8(&pFlic_info->data);
             // argb
 #if BR_ENDIAN_BIG
-            palette_pixels[3] = green;
-            palette_pixels[2] = blue;
-            palette_pixels[1] = red;
-            palette_pixels[0] = 0;
+            *palette_pixels++ = 0;
+            *palette_pixels++ = red;
+            *palette_pixels++ = green;
+            *palette_pixels++ = blue;
 #else
-            palette_pixels[0] = blue;
-            palette_pixels++;
-            palette_pixels[0] = green;
-            palette_pixels++;
-            palette_pixels[0] = red;
-            palette_pixels++;
-            palette_pixels[0] = 0;
-            palette_pixels++;
+            *palette_pixels++ = blue;
+            *palette_pixels++ = green;
+            *palette_pixels++ = red;
+            *palette_pixels++ = 0;
 #endif
-            // LOG_DEBUG("color %d", current_colour);
         }
         if (!gPalette_fuck_prevention) {
             DRSetPaletteEntries(gPalette, current_colour, change_count);
@@ -2157,7 +2149,6 @@ void LoadInterfaceStrings(void) {
         fclose(f);
 #endif
     }
-
 }
 
 // IDA: void __cdecl FlushInterfaceFonts()
