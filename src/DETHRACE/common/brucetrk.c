3a900ca0b500ce06c4b142240f9359dcfe3b8f0c
Jeff H <jeff@1amstudios.com>
2026-04-10 18:09:20 +1200

    Cleanup StripBlendedFaces
    AI generated

--- src/DETHRACE/common/brucetrk.c
+++ src/DETHRACE/common/brucetrk.c
@@ -139,7 +139,11 @@ void StripBlendedFaces(br_actor* pActor, br_model* pModel) {
             memcpy(&gMr_blendy->model->faces[gMr_blendy->model->nfaces], face, sizeof(br_face));
             gMr_blendy->model->nfaces++;
             if (i < (pModel->nfaces - 1)) {
+#ifdef DETHRACE_FIX_BUGS
+                memmove(pModel->faces + i, pModel->faces + i + 1, (pModel->nfaces - i - 1) * sizeof(br_face));
+#else
                 ((void*(__cdecl*)(void*, const void*, size_t))memcpy)(pModel->faces + i, pModel->faces + i + 1, (pModel->nfaces - i - 1) * sizeof(br_face));
+#endif
             }
             pModel->nfaces--;
             changed_one = 1;
